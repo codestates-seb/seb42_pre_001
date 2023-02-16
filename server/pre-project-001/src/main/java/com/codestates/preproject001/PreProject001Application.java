e3e5401306f47eb80e4947ac6f26d553fa86041e
Taeyang-Jin <wlsxodid0407@gmail.com>
2023-02-16 10:51:24 +0900

    init

---
+++ server/pre-project-001/src/main/java/com/codestates/preproject001/PreProject001Application.java
@@ -0,0 +1,13 @@
+package com.codestates.preproject001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreProject001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreProject001Application.class, args);
+	}
+
+}
