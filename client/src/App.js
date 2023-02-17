89fee53a8f42188f6ce13c188fd30a415681232c
myungju <myungju030@gmail.com>
2023-02-17 20:12:15 +0900

    fix: 충돌 해결

--- client/src/App.js
+++ client/src/App.js
@@ -5,17 +5,32 @@ import Inquiry from './pages/Inquiry';
 import Tages from './pages/Tags';
 import Users from './pages/Users';
 import Companies from './pages/Companies';
+import { createGlobalStyle } from 'styled-components';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="/inquiry" element={<Inquiry />}></Route>
-      <Route path="/tags" element={<Tages />}></Route>
-      <Route path="/users" element={<Users />}></Route>
-      <Route path="/companies" element={<Companies />}></Route>
-    </Routes>
+    <>
+      <GlobalStyle></GlobalStyle>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/inquiry" element={<Inquiry />}></Route>
+        <Route path="/tags" element={<Tages />}></Route>
+        <Route path="/users" element={<Users />}></Route>
+        <Route path="/companies" element={<Companies />}></Route>
+      </Routes>
+    </>
   );
 }
 
 export default App;
+
+const GlobalStyle = createGlobalStyle`
+  body {box-sizing: border-box;
+  width: 100vw;
+  height: 100vh;  
+  background-color: hsl(210, 8%, 93%);
+  }
+  a {
+    text-decoration: none;
+  }
+`;
