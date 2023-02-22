be4d502ce2983e1553d2a61da0a8bb9cd0cf5d57
Minhyung Lee <freeman99000@gmail.com>
2023-02-22 10:15:50 +0900

    "fix : 로그인 코드 수정"

--- client/src/components/login/LoginContainer.js
+++ client/src/components/login/LoginContainer.js
@@ -39,7 +39,6 @@ export default function Login() {
           headers: {
             'Content-Type': 'application/json',
           },
-          withCredentials: true,
         }
       );
       console.log(body);
