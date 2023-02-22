d1dffd37e813fe95c25f29204ffa7b4f7ac57345
SangHyuk <sanggur591@naver.com>
2023-02-22 14:54:28 +0900

    test

--- server/pre-project-001/src/main/java/com/codestates/preproject001/oath/SecurityConfiguration.java
+++ server/pre-project-001/src/main/java/com/codestates/preproject001/oath/SecurityConfiguration.java
@@ -45,6 +45,7 @@ public class SecurityConfiguration {
                         .antMatchers(HttpMethod.OPTIONS).permitAll()
                         .anyRequest().permitAll()
                 );
+
         return http.build();
     }
 
