6264b1352090b7fad56edfaa709c6795c5e7bae2
myungju <myungju030@gmail.com>
2023-02-27 10:24:58 +0900

    fix: 충돌 해결

--- client/src/components/question/QuestionContent.js
+++ client/src/components/question/QuestionContent.js
@@ -4,6 +4,8 @@ import ViewProfile from '../ViewProfile';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import ViewTags from '../ViewTags';
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+import prism from 'react-syntax-highlighter/dist/esm/styles/prism/prism';
 import { setContent } from '../../slice/questionSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import { useCookies } from 'react-cookie';
@@ -52,9 +54,83 @@ const QuestionContent = ({ question, user, tags }) => {
         <VoteDownButton size="45px"></VoteDownButton>
       </VoteContainer>
       <ContentContainer>
-        <ReactMarkdown remarkPlugins={[remarkGfm]}>
+        <Preview
+          remarkPlugins={[remarkGfm]}
+          components={{
+            code({ inline, className, children, ...props }) {
+              const match = /language-(\w+)/.exec(className || '');
+              return inline ? (
+                // 강조 (``)
+                <code
+                  style={{
+                    background:
+                      'linear-gradient( to right, var(--sub-highlight-color) 15%, var(--highlight-color) 85%, var(--sub-highlight-color) )',
+                    padding: '2px',
+                    borderRadius: '3px',
+                  }}
+                  {...props}
+                >
+                  {children}
+                </code>
+              ) : match ? (
+                // 코드 (```)
+                <SyntaxHighlighter
+                  style={prism}
+                  language={match[1]}
+                  PreTag="div"
+                  {...props}
+                >
+                  {String(children)
+                    .replace(/\n$/, '')
+                    .replace(/\n&nbsp;\n/g, '')
+                    .replace(/\n&nbsp\n/g, '')}
+                </SyntaxHighlighter>
+              ) : (
+                <SyntaxHighlighter
+                  style={prism}
+                  language="textile"
+                  PreTag="div"
+                  {...props}
+                >
+                  {String(children).replace(/\n$/, '')}
+                </SyntaxHighlighter>
+              );
+            },
+            // 인용문 (>)
+            blockquote({ children, ...props }) {
+              return (
+                <div
+                  style={{
+                    background: '#f0f0f0',
+                    padding: '1px 15px',
+                    borderRadius: '10px',
+                  }}
+                  {...props}
+                >
+                  {children}
+                </div>
+              );
+            },
+            img({ ...props }) {
+              return (
+                <img
+                  style={{ maxWidth: '60vw' }}
+                  src={props.src.replace('../../../../public/', '/')}
+                  alt="MarkdownRenderer__Image"
+                />
+              );
+            },
+            em({ children, ...props }) {
+              return (
+                <span style={{ fontStyle: 'italic' }} {...props}>
+                  {children}
+                </span>
+              );
+            },
+          }}
+        >
           {question.content}
-        </ReactMarkdown>
+        </Preview>
         <ViewTags tags={tags} />
         <ButtonsAndProfile>
           <InquiryButtons editFunction={editPost} deleteFunction={deletePost} />
@@ -101,4 +177,11 @@ const ButtonsAndProfile = styled.div`
   display: flex;
   justify-content: space-between;
   margin: 16px 0;
+`;
+const Preview = styled(ReactMarkdown)`
+  white-space: normal;
+  word-wrap: break-word;
+  > p {
+    margin-bottom: 16.5px;
+  }
 `;
