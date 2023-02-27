e7ee0dbb39b8f64b2d5c3b581efde798a5db8f35
SangHyuk <sanggur591@naver.com>
2023-02-27 15:47:05 +0900

    share

--- server/pre-project-001/src/main/java/com/codestates/preproject001/answer/dto/AnswerMyPageDto.java
+++ server/pre-project-001/src/main/java/com/codestates/preproject001/answer/dto/AnswerMyPageDto.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 @Setter
 public class AnswerMyPageDto { // 마이페이지 answer 목록에는 답변한 질문의 제목, id, 작성시간이 있길래 이거만 따로 받아오기 위함..
     @ApiModelProperty(required=true)
-    private long questionId;
+    private long answerId;
     private String title;
     private LocalDateTime createdAt;
     private long answerCount;
