5e86f50c74261a02707c705db9b00d3a77dc217d
myungju <myungju030@gmail.com>
2023-02-27 10:47:25 +0900

    feat: 마크다운 컴포넌트 생성

--- client/src/components/question/QuestionContent.js
+++ client/src/components/question/QuestionContent.js
@@ -1,17 +1,14 @@
 import styled from 'styled-components';
 import { GoTriangleUp, GoTriangleDown } from 'react-icons/go';
 import ViewProfile from '../ViewProfile';
-import ReactMarkdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
 import ViewTags from '../ViewTags';
-import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
-import prism from 'react-syntax-highlighter/dist/esm/styles/prism/prism';
 import { setContent } from '../../slice/questionSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import { useCookies } from 'react-cookie';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import InquiryButtons from '../inquiry/InquiryButtons';
+import Markdown from '../Markdown';
 const QuestionContent = ({ question, user, tags }) => {
   const [cookie] = useCookies();
   const state = useSelector((state) => state.login);
@@ -54,83 +51,7 @@ const QuestionContent = ({ question, user, tags }) => {
         <VoteDownButton size="45px"></VoteDownButton>
       </VoteContainer>
       <ContentContainer>
-        <Preview
-          remarkPlugins={[remarkGfm]}
-          components={{
-            code({ inline, className, children, ...props }) {
-              const match = /language-(\w+)/.exec(className || '');
-              return inline ? (
-                // 강조 (``)
-                <code
-                  style={{
-                    background:
-                      'linear-gradient( to right, var(--sub-highlight-color) 15%, var(--highlight-color) 85%, var(--sub-highlight-color) )',
-                    padding: '2px',
-                    borderRadius: '3px',
-                  }}
-                  {...props}
-                >
-                  {children}
-                </code>
-              ) : match ? (
-                // 코드 (```)
-                <SyntaxHighlighter
-                  style={prism}
-                  language={match[1]}
-                  PreTag="div"
-                  {...props}
-                >
-                  {String(children)
-                    .replace(/\n$/, '')
-                    .replace(/\n&nbsp;\n/g, '')
-                    .replace(/\n&nbsp\n/g, '')}
-                </SyntaxHighlighter>
-              ) : (
-                <SyntaxHighlighter
-                  style={prism}
-                  language="textile"
-                  PreTag="div"
-                  {...props}
-                >
-                  {String(children).replace(/\n$/, '')}
-                </SyntaxHighlighter>
-              );
-            },
-            // 인용문 (>)
-            blockquote({ children, ...props }) {
-              return (
-                <div
-                  style={{
-                    background: '#f0f0f0',
-                    padding: '1px 15px',
-                    borderRadius: '10px',
-                  }}
-                  {...props}
-                >
-                  {children}
-                </div>
-              );
-            },
-            img({ ...props }) {
-              return (
-                <img
-                  style={{ maxWidth: '60vw' }}
-                  src={props.src.replace('../../../../public/', '/')}
-                  alt="MarkdownRenderer__Image"
-                />
-              );
-            },
-            em({ children, ...props }) {
-              return (
-                <span style={{ fontStyle: 'italic' }} {...props}>
-                  {children}
-                </span>
-              );
-            },
-          }}
-        >
-          {question.content}
-        </Preview>
+        <Markdown content={question.content} />
         <ViewTags tags={tags} />
         <ButtonsAndProfile>
           <InquiryButtons editFunction={editPost} deleteFunction={deletePost} />
@@ -177,11 +98,4 @@ const ButtonsAndProfile = styled.div`
   display: flex;
   justify-content: space-between;
   margin: 16px 0;
-`;
-const Preview = styled(ReactMarkdown)`
-  white-space: normal;
-  word-wrap: break-word;
-  > p {
-    margin-bottom: 16.5px;
-  }
 `;
