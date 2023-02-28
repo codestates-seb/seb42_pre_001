b037a0f87577b215e25987629a0b9ab6dcacff71
qwerty00ui88 <qwerty0.0ui88@gmail.com>
2023-02-28 09:34:44 +0900

    feat: update, delete 함수 작성

--- server/pre-project-001/src/main/java/com/codestates/preproject001/member/controller/MemberController.java
+++ server/pre-project-001/src/main/java/com/codestates/preproject001/member/controller/MemberController.java
@@ -21,12 +21,11 @@ import javax.validation.Valid;
 import javax.validation.constraints.Positive;
 import java.util.List;
 
-
 @Api(tags = "Member Controller")
 @RestController
 @RequestMapping("/members")
 @Validated
-public class MemberController { //1차 완료
+public class MemberController { // 1차 완료
     private final MemberService memberService;
     private final MemberMapper mapper;
 
@@ -43,9 +42,9 @@ public class MemberController { //1차 완료
     }
 
     @ApiOperation(value = "프로필 고치기")
-    @PatchMapping//프로필 고치기
+    @PatchMapping // 프로필 고치기
     public ResponseEntity patchMyProfile(@AuthenticationPrincipal MemberDetails memberDetails,
-                                      @Valid @RequestBody MemberPatchDto memberPatchDto) {
+            @Valid @RequestBody MemberPatchDto memberPatchDto) {
         Member member = mapper.memberPatchDtoToMember(memberPatchDto);
         member.setMemberId(memberDetails.getMemberId());
         memberService.updateMember(member);
@@ -54,23 +53,23 @@ public class MemberController { //1차 완료
     }
 
     @ApiOperation(value = "멤버 페이지로 가기")
-    @GetMapping("/{member-id}") //멤버 페이지로 가게함 (마이페이지)
+    @GetMapping("/{member-id}") // 멤버 페이지로 가게함 (마이페이지)
     public ResponseEntity getMember(@PathVariable("member-id") long memberId) {
         Member dbMember = memberService.findMember(memberId);
-        return new ResponseEntity<> (new SingleResponseDto<>(mapper.memberToMemberMyPageDto(dbMember)),HttpStatus.OK);
+        return new ResponseEntity<>(new SingleResponseDto<>(mapper.memberToMemberMyPageDto(dbMember)), HttpStatus.OK);
     }
 
-
     @ApiOperation(value = "users목록 가기")
-    @GetMapping //user목록을 띄워준다
+    @GetMapping // user목록을 띄워준다
     public ResponseEntity getMembers(@Positive @RequestParam int page) {
         Page<Member> members = memberService.findMembers(page - 1);
         List<Member> content = members.getContent();
-        return new ResponseEntity<>(new MultiResponseDto<>(mapper.membersToMemberResponseDtos(content), members), HttpStatus.OK);
+        return new ResponseEntity<>(new MultiResponseDto<>(mapper.membersToMemberResponseDtos(content), members),
+                HttpStatus.OK);
     }
 
     @ApiOperation(value = "회원탈퇴 (상태만변경)")
-    @DeleteMapping //memberStatus를 deleted로 변경(삭제) + 쿼리문으로 이메일찾기+삭제로변경 한번에 날려주기
+    @DeleteMapping // memberStatus를 deleted로 변경(삭제) + 쿼리문으로 이메일찾기+삭제로변경 한번에 날려주기
     public ResponseEntity deleteMember(@AuthenticationPrincipal MemberDetails memberDetails) {
 
         Member member = memberService.findVerifiedMember(memberDetails.getMemberId());
